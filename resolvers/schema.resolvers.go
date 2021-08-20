c84346381045d6ff58334cef462b5c3dbe4308ee
Kiwi <agiannoulis@mavestro.co>
2021-08-20 13:16:21 +0300

    Refactor GraphQL resolvers to use scheduler methods instead of asserting redis existence

--- resolvers/schema.resolvers.go
+++ resolvers/schema.resolvers.go
@@ -5,18 +5,18 @@ package resolvers
 
 import (
 	"context"
-	"fmt"
 	"github.com/ProjectAthenaa/scheduling-service/graph/generated"
 	"github.com/ProjectAthenaa/scheduling-service/graph/model"
 	"github.com/ProjectAthenaa/scheduling-service/scheduler"
 	module "github.com/ProjectAthenaa/sonic-core/protos"
 	"github.com/ProjectAthenaa/sonic-core/sonic"
-	"github.com/ProjectAthenaa/sonic-core/sonic/core"
 	tasks "github.com/ProjectAthenaa/sonic-core/task_controller"
 )
 
 func (r *mutationResolver) SendCommand(ctx context.Context, controlToken string, command model.Command) (bool, error) {
-	core.Base.GetRedis("cache").Publish(ctx, fmt.Sprintf("tasks:command:%s", controlToken), command)
+	if err := scheduler.PublishCommand(ctx, controlToken, command); err != nil {
+		return false, err
+	}
 	return true, nil
 }
 
@@ -27,11 +27,12 @@ func (r *queryResolver) GetScheduledTasks(ctx context.Context) ([]*model.Task, e
 
 func (r *subscriptionResolver) TaskUpdates(ctx context.Context, subscriptionToken string) (<-chan *model.TaskStatus, error) {
 	updates := make(chan *model.TaskStatus)
-	pubSub := core.Base.GetRedis("cache").Subscribe(ctx, fmt.Sprintf("tasks:updates:%s", subscriptionToken))
-
+	pubSub, err := scheduler.Subscribe(ctx, subscriptionToken)
+	if err != nil {
+		return nil, err
+	}
 	go func() {
 		var status tasks.Status
-		var err error
 		defer pubSub.Close()
 		for update := range pubSub.Channel() {
 			if err = json.Unmarshal([]byte(update.Payload), &status); err != nil {
--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,6 +1,13 @@
 package scheduler
 
-import "github.com/ProjectAthenaa/scheduling-service/graph/model"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/ProjectAthenaa/scheduling-service/graph/model"
+	"github.com/ProjectAthenaa/sonic-core/sonic/core"
+	"github.com/go-redis/redis/v8"
+)
 
 var scheduler = NewScheduler()
 
@@ -16,4 +23,33 @@ func GetUserTasks(userID string) []*model.Task {
 //Stop, stops the scheduler
 func Stop() {
 	scheduler.cancelFunc()
+}
+
+func Subscribe(ctx context.Context, token string) (*redis.PubSub, error) {
+	scheduler.locker.Lock()
+	defer scheduler.locker.Unlock()
+	for _, tks := range scheduler.data {
+		for _, tk := range tks {
+			if tk.subscriptionToken == token {
+				return core.Base.GetRedis("cache").Subscribe(ctx, fmt.Sprintf("tasks:updates:%s", token)), nil
+			}
+		}
+	}
+
+	return nil, errors.New("task_not_found")
+}
+
+func PublishCommand(ctx context.Context, token string, command model.Command) error {
+	scheduler.locker.Lock()
+	defer scheduler.locker.Unlock()
+	for _, tks := range scheduler.data {
+		for _, tk := range tks {
+			if tk.controlToken == token {
+				core.Base.GetRedis("cache").Publish(ctx, fmt.Sprintf("tasks:command:%s", token), command)
+				return nil
+			}
+		}
+	}
+
+	return errors.New("task_not_found")
 }
