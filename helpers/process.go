86336b2bbc06cfe08d0e41b38185d9b3d459047e
Kiwi <agiannoulis@mavestro.co>
2021-09-03 14:00:14 +0300

    depreceated use of task controller

--- helpers/process.go
+++ helpers/process.go
@@ -10,11 +10,19 @@ import (
 func GetCurrentProcessNumber() int {
 	c := os.Getenv("COUNT")
 	count, _ := strconv.Atoi(c)
+	if count == 0 {
+		os.Setenv("COUNT", "1")
+		return 1
+	}
 	return count
 }
 
 func GetProcessCount() int {
 	val, _ := core.Base.GetRedis("cache").Get(context.Background(), "schedulers").Result()
 	count, _ := strconv.Atoi(val)
+	if count == 0 {
+		core.Base.GetRedis("cache").Incr(context.Background(), "schedulers")
+		return 1
+	}
 	return count
 }
