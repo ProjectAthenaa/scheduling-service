48fb9beb3a7046e397b1c03d24885bbd40df014e
Kiwi <agiannoulis@mavestro.co>
2021-09-10 06:14:28 +0300

    huge refactor

--- scheduler/helpers.go
+++ scheduler/helpers.go
@@ -1,6 +1,14 @@
 package scheduler
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"github.com/ProjectAthenaa/scheduling-service/helpers"
+	"github.com/ProjectAthenaa/sonic-core/sonic"
+	"github.com/ProjectAthenaa/sonic-core/sonic/core"
+	task2 "github.com/ProjectAthenaa/sonic-core/sonic/database/ent/task"
+	"github.com/google/uuid"
+	"sync"
+)
 
 //removeTask removes a given task from the index provided
 func removeTask(t []*Task, i int) []*Task {
@@ -12,4 +20,53 @@ func removeTask(t []*Task, i int) []*Task {
 func removeID(t []uuid.UUID, i int) []uuid.UUID {
 	t[i] = t[len(t)-1]
 	return t[:len(t)-1]
+}
+
+func removeFromProcessingList(taskID string) {
+	core.Base.GetRedis("cache").SRem(context.Background(), "scheduler:processing", taskID)
+}
+
+func (s *Schedule) loadTask(taskID string) *Task  {
+	dbTask, err := core.Base.GetPg("pg").
+		Task.
+		Query().
+		WithProfileGroup().
+		WithProduct().
+		WithProxyList().
+		WithTaskGroup().
+		Where(
+			task2.ID(
+				sonic.UUIDParser(taskID),
+			),
+		).
+		First(s.ctx)
+
+	if err != nil {
+		removeFromProcessingList(taskID)
+		return nil
+	}
+
+	user, err := dbTask.
+		QueryProfileGroup().
+		QueryApp().
+		QueryUser().
+		First(s.ctx)
+
+	ctx, cancel := context.WithCancel(s.ctx)
+
+	task := &Task{
+		Task:              dbTask,
+		subscriptionToken: dbTask.ID.String(),
+		controlToken:      helpers.SHA1(dbTask.ID.String()),
+		taskID:            dbTask.ID.String(),
+		userID:            user.ID.String(),
+		startMutex:        &sync.Mutex{},
+		dataLock:          &sync.Mutex{},
+		site:              dbTask.Edges.Product[0].Site,
+		ctx:               ctx,
+		cancel:            cancel,
+		startTime:         *dbTask.StartTime,
+	}
+
+	return task
 }
--- scheduler/schedule.go
+++ scheduler/schedule.go
@@ -3,18 +3,14 @@ package scheduler
 import (
 	"context"
 	"github.com/ProjectAthenaa/scheduling-service/graph/model"
-	"github.com/ProjectAthenaa/scheduling-service/helpers"
 	"github.com/ProjectAthenaa/sonic-core/sonic/core"
-	"github.com/ProjectAthenaa/sonic-core/sonic/database/ent/task"
-	"github.com/google/uuid"
 	"github.com/prometheus/common/log"
 	"sync"
 	"time"
 )
 
 type Schedule struct {
-	data       map[time.Time][]uuid.UUID
-	tasks      map[uuid.UUID]*Task
+	data       map[time.Time][]*Task
 	locker     *sync.Mutex
 	ctx        context.Context
 	cancelFunc context.CancelFunc
@@ -28,8 +24,7 @@ func NewScheduler() *Schedule {
 
 //init initializes the scheduler by creating a new data map, populating the map and processing the tasks
 func (s *Schedule) init() {
-	s.data = map[time.Time][]uuid.UUID{}
-	s.tasks = map[uuid.UUID]*Task{}
+	s.data = map[time.Time][]*Task{}
 	s.locker = &sync.Mutex{}
 
 	go func() {
@@ -49,20 +44,25 @@ func (s *Schedule) init() {
 
 			//start tasks
 
-			for _, t := range s.getChunks() {
-				if t.taskStarted {
-					continue
-				}
-				if err := t.start(s.ctx); err != nil {
-					log.Error("error starting task", err, "task_id:", t.ID.String())
+			for startTime, tasks := range s.data {
+				if time.Now().Sub(startTime) >= time.Second*2 {
+					for _, tk := range tasks {
+						if tk.taskStarted {
+							continue
+						}
+						if err := tk.start(s.ctx); err != nil {
+							log.Error("error starting task", err, "task_id: ", tk.ID.String())
+						}
+					}
 				}
 			}
+
 		}
 	}()
 }
 
 //add appends the task to the appropriate task slice in data
-func (s *Schedule) add(task *Task) {
+func (s *Schedule) add(taskID string) {
 	select {
 	case <-s.ctx.Done():
 		return
@@ -70,17 +70,19 @@ func (s *Schedule) add(task *Task) {
 		break
 	}
 
+	task := s.loadTask(taskID)
+
 	s.locker.Lock()
 	defer s.locker.Unlock()
 
 	//loop through the data to check if task already exists
 	for t, ids := range s.data {
-		for i, id := range ids {
-			if tk := s.tasks[id]; tk.ID == task.ID && tk.StartTime == task.StartTime {
+		for i, tk := range ids {
+			if tk.ID == task.ID && tk.StartTime == task.StartTime {
 				tk = task
 				return
 			} else if tk != nil {
-				s.data[t] = removeID(s.data[t], i)
+				s.data[t] = removeTask(s.data[t], i)
 				goto addTask
 			}
 		}
@@ -89,11 +91,9 @@ func (s *Schedule) add(task *Task) {
 		}
 	}
 
-
 	//append task to the correct data slice
 addTask:
-	s.data[*task.StartTime] = append(s.data[*task.StartTime], task.ID)
-	s.tasks[task.ID] = task
+	s.data[*task.StartTime] = append(s.data[*task.StartTime], task)
 	go task.getPayload()
 }
 
@@ -116,33 +116,6 @@ func (s *Schedule) deleteOlderEntries() {
 	}
 }
 
-//getChunks returns the tasks assigned to the current process
-func (s *Schedule) getChunks() []*Task {
-	select {
-	case <-s.ctx.Done():
-		return nil
-	default:
-		break
-	}
-	var tasks []*Task
-	s.locker.Lock()
-	defer s.locker.Unlock()
-
-	for k := range s.data {
-		if k.Sub(time.Now()) <= time.Second*1 {
-			var timeTasks []*Task
-			for _, id := range s.data[k] {
-				timeTasks = append(timeTasks, s.tasks[id])
-			}
-
-			chunks := chunk(timeTasks, helpers.GetProcessCount())
-			tasks = append(tasks, chunks[helpers.GetCurrentProcessNumber()]...)
-		}
-	}
-
-	return tasks
-}
-
 //startMonitors starts the monitors for each task after first isolating the unique tasks
 func (s *Schedule) startMonitors() {
 	select {
@@ -157,20 +130,18 @@ func (s *Schedule) startMonitors() {
 	for k := range s.data {
 		if k.Sub(time.Now()) <= time.Minute {
 			var uniqueTasks sync.Map
-			chunks := chunk(s.getTasks(k), 4)
-
 			var wg sync.WaitGroup
 
-			for i := 0; i < len(chunks); i++ {
+			tasks := s.getTasks(k)
+
+			for _, tk := range tasks {
 				wg.Add(1)
-				index := i
+				tk := tk
 				go func() {
 					defer wg.Done()
-					for _, tk := range chunks[index] {
-						mID := tk.getMonitorID()
-						tk.monitorChannel = mID
-						uniqueTasks.Store(mID, tk)
-					}
+					mID := tk.getMonitorID()
+					tk.monitorChannel = mID
+					uniqueTasks.Store(mID, tk)
 				}()
 			}
 
@@ -184,14 +155,14 @@ func (s *Schedule) startMonitors() {
 						return true
 					}
 					tk.monitorStarted = true
+				} else {
+					return true
 				}
 
 				go func() {
-					for _, ch := range chunks {
-						for _, t := range ch {
-							if t.monitorChannel == tk.monitorChannel {
-								t.monitorStarted = true
-							}
+					for _, tsk := range tasks {
+						if tsk.monitorChannel == tk.monitorChannel {
+							tsk.monitorStarted = true
 						}
 					}
 				}()
@@ -206,97 +177,13 @@ func (s *Schedule) startMonitors() {
 //populate spawns the deleteOlderEntries func as a goroutine, creates a goroutine to listen for deleted tasks, and retrieves
 //all tasks from the database
 func (s *Schedule) populate() {
-	go s.deleteOlderEntries()
-
-	go func() {
-		pubSub := core.Base.GetRedis("cache").Subscribe(s.ctx, "scheduler:tasks-deleted")
-		defer pubSub.Close()
-		for taskID := range pubSub.Channel() {
-			s.locker.Lock()
-			for k, ids := range s.data {
-				for i, id := range ids {
-					if tk := s.tasks[id]; tk.ID.String() == taskID.Payload {
-						delete(s.tasks, id)
-						s.data[k] = removeID(ids, i)
-
-						if tk.taskStarted {
-							tk.stop()
-						}
-					}
-				}
-			}
-			s.locker.Unlock()
-		}
-	}()
-
-	for range time.Tick(time.Millisecond * 200) {
-		select {
-		case <-s.ctx.Done():
-			return
-		default:
-			break
-		}
-		tasks, err := core.
-			Base.
-			GetPg("pg").
-			Task.Query().
-			Where(
-				task.StartTimeGTE(
-					time.Now(),
-				),
-				task.StartTimeLTE(time.Now().Add(time.Minute*30)),
-				//task.StartTimeNotNil(),
-			).
-			WithProduct().
-			WithProfileGroup().
-			WithProxyList().
-			All(s.ctx)
-		if err != nil {
-			continue
-		}
-
-		var wg sync.WaitGroup
-		for _, tk := range tasks {
-			if tk.StartTime == nil {
-				continue
-			}
-
-			wg.Add(1)
-
-			t := tk
-			go func() {
-				defer wg.Done()
-				user, err := t.
-					QueryProfileGroup().
-					QueryApp().
-					QueryUser().
-					First(s.ctx)
-				if err != nil {
-					if user == nil {
-						core.Base.GetPg("pg").Task.DeleteOne(t).ExecX(context.Background())
-					}
-					log.Error("error getting user ", err)
-					return
-				}
-				c, cancel := context.WithCancel(context.Background())
-
-				s.add(&Task{
-					Task:              t,
-					subscriptionToken: t.ID.String(),
-					controlToken:      helpers.SHA1(t.ID.String()),
-					taskID:            t.ID.String(),
-					userID:            user.ID.String(),
-					startMutex:        &sync.Mutex{},
-					dataLock:          &sync.Mutex{},
-					site:              t.Edges.Product[0].Site,
-					ctx:               c,
-					cancel:            cancel,
-					startTime:         *t.StartTime,
-				})
-			}()
-		}
-		wg.Wait()
+	rdb := core.Base.GetRedis("cache")
+	for range time.Tick(time.Millisecond * 25) {
+		newTask := rdb.SPop(s.ctx, "scheduler:scheduled").Val()
+		rdb.SAdd(s.ctx, "scheduler:processing", newTask)
+		go s.add(newTask)
 	}
+
 }
 
 //getUserTasks retrieves the current user tasks that are appended to the data pool
@@ -305,11 +192,11 @@ func (s *Schedule) getUserTasks(userID string) (tasks []*model.Task) {
 	defer s.locker.Unlock()
 	for k, v := range s.data {
 		for _, t := range v {
-			if tk := s.tasks[t]; tk.userID == userID {
+			if t.userID == userID {
 				tasks = append(tasks, &model.Task{
-					ID:                tk.taskID,
-					SubscriptionToken: tk.subscriptionToken,
-					ControlToken:      tk.controlToken,
+					ID:                t.taskID,
+					SubscriptionToken: t.subscriptionToken,
+					ControlToken:      t.controlToken,
 					StartTime:         k,
 				})
 			}
@@ -325,9 +212,9 @@ func (s *Schedule) getData() map[time.Time][]*Task {
 
 	var tasks = map[time.Time][]*Task{}
 
-	for k, ids := range s.data {
-		for _, id := range ids {
-			tasks[k] = append(tasks[k], s.tasks[id])
+	for k, tks := range s.data {
+		for _, tk := range tks {
+			tasks[k] = append(tasks[k], tk)
 		}
 	}
 
@@ -336,9 +223,9 @@ func (s *Schedule) getData() map[time.Time][]*Task {
 
 func (s *Schedule) getTasks(t time.Time) []*Task {
 	var tasks []*Task
-	if ids, ok := s.data[t]; ok {
-		for _, id := range ids {
-			tasks = append(tasks, s.tasks[id])
+	if tks, ok := s.data[t]; ok {
+		for _, tk := range tks {
+			tasks = append(tasks, tk)
 		}
 	}
 	return tasks
